52bd7a4a1b0ff6fbc996d373ce2f5c7cb3077d6b
KagisoMokgobi0123 <162275760+KagisoMokgobi0123@users.noreply.github.com>
2025-09-20 23:35:56 +0200

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,1 +1,37 @@
 
+#Activity 1: designing a class
+
+#class
+class Vehicle:
+	#attribute(private) and a constructor method
+    def __init__(self,color):
+    	self.color = "Red"          
+   	def model(self):
+    	print("Audi")
+
+#inherit a vehicle class
+class Car(Vehicle):
+    def drive(self):
+    	#print color of a car
+        return self.color
+        
+#objects        
+car = Car(Vehicle)
+vehicle = Vehicle()
+
+print(car.drive())
+vehicle.model()
+
+
+#Activity 2: Polymorphism
+#Class
+class Horse:
+	def move(self):
+    	return "Horse move with four feet."
+        
+class Chicken:
+	def move(self):
+    	return "Chicken move with 2 feet."
+        
+for animal in [Horse(),Chicken()]
+	print(animal.move())
